a07490941c3814581471db4310a16ae945765df2
hnust_hua <2359923694@qq.com>
2019-02-22 11:16:08 +0800

    quartz 简单demo

---
+++ src/main/java/com/quartz/QuartzDemo.java
@@ -0,0 +1,17 @@
+package com.quartz;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by Administrator on 2019/2/20.
+ */
+public class QuartzDemo {
+
+    Logger logger = LoggerFactory.getLogger(QuartzDemo.class);
+
+    public void test(){
+        logger.info("quartz test");
+    }
+
+}
